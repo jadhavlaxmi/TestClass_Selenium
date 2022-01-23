17864ca3acd1198c66fa2c0b0ea57d738108a91b
jadhavlaxmi <jadhavlaxmi108@gmail.com>
2022-01-23 22:39:26 +0530

    added file

---
+++ src/test/java/Capabilities_Synchronisation/Synchronisation_4_2.java
@@ -0,0 +1,63 @@
+package Capabilities_Synchronisation;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Synchronisation_4_2 {
+	
+	WebDriver driver;
+	WebDriverWait wait;
+	Actions act;
+
+	@Before
+	public void setUp()
+	{
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.in/");
+		
+		wait = new WebDriverWait(driver, 15);
+		
+		act = new Actions(driver);
+	}
+	
+	@Test
+	public void dropDown() throws InterruptedException
+	{
+		
+		WebElement helloSignInButton = driver.findElement(By.xpath("//span[contains(text(),'Hello, Sign in')]"));
+		act.moveToElement(helloSignInButton).perform();
+		
+		List<WebElement> yourAccountOptions = driver.findElements(By.xpath("//div[@id='nav-al-your-account']/a/span"));
+		wait.until(ExpectedConditions.visibilityOfAllElements(yourAccountOptions));
+		System.out.println("After mouse hover on Hello, \"Sign In\" Your Account List contains total of Options in number as - " + helloSignInButton.getSize());
+		System.out.println("And the List options available as below - ");
+		for (int i = 0; i < yourAccountOptions.size(); i++) {
+			System.out.println((i+1) + " " + yourAccountOptions.get(i).getText());
+		}	
+	}
+	
+	@After
+	public void teardown() throws InterruptedException
+	{
+		// To Halt the execution for sometime 
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+
+}
---
+++ src/test/java/Capabilities_Synchronisation/ToolTipHandling_1.java
@@ -0,0 +1,62 @@
+package Capabilities_Synchronisation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ToolTipHandling_1 {
+
+	WebDriver driver;
+
+	@Before
+	public void setUp()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://demoqa.com/tool-tips/");
+	}
+
+	@Test
+	public void dropDown() throws InterruptedException
+	{
+			
+		WebElement hoverButton = driver.findElement(By.id("toolTipButton"));
+
+		// To Halt the execution for sometime 
+		Thread.sleep(3000);
+
+		Actions act = new Actions(driver);
+		act.moveToElement(hoverButton).build().perform();
+
+		// To Halt the execution for sometime 
+		Thread.sleep(3000);
+
+		WebElement suggestionTextElement = driver.findElement(By.xpath("//div[text()='You hovered over the Button']"));
+		String suggestionText = suggestionTextElement.getText();
+		System.out.println("Suggestion Box text is : " + suggestionText);
+		
+		Assert.assertEquals("You hovered over the Button", suggestionText);
+
+		
+	}
+
+	@After
+	public void teardown() throws InterruptedException
+	{
+		// To Halt the execution for sometime 
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
